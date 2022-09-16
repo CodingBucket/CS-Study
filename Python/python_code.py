e1a9669d774b5abb0102241b7256d8022039ea61
Hasan Khan <hasan.khan.uap@gmail.com>
2022-09-17 01:16:31 +0600

    Create python_code.py

---
+++ Python/python_code.py
@@ -0,0 +1,4 @@
+
+# List comprehensions are an elegant way to build a list without having to use different for loops to append values one by one.
+list_com = [x for x in range(10) if x % 3 == 0]  # Output => [0, 3, 6, 9]
+
