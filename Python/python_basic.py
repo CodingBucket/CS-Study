3383e5f3500a4c15e653f31faefab35e8673e48a
Hasan Khan <hasan.khan.uap@gmail.com>
2022-09-17 05:50:07 +0600

    Update python_basic.py

--- Python/python_basic.py
+++ Python/python_basic.py
@@ -2,3 +2,7 @@
 # List comprehensions are an elegant way to build a list without having to use different for loops to append values one by one.
 list_com = [x for x in range(10) if x % 3 == 0]  # Output => [0, 3, 6, 9]
 
+# Print 2 digit after point
+print("%.2f" % 10)
+
+
