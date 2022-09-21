9d477c7f90d1b6c76ea4badec0f270776ef5f8cd
Hasan Khan <hasan.khan.uap@gmail.com>
2022-09-22 02:10:27 +0600

    Update python_basic.py

--- Python/python_basic.py
+++ Python/python_basic.py
@@ -1,10 +1,3 @@
-# In Python 3 you can iterate over the lines of standard input, the loop will stop when EOF is reached:
-from sys import stdin
-
-for line in stdin:
-  print(line, end='')
-  
-  
 # List comprehensions are an elegant way to build a list without having to use different for loops to append values one by one.
 list_com = [x for x in range(10) if x % 3 == 0]  # Output => [0, 3, 6, 9]
 
@@ -15,6 +8,12 @@ print("%.2f" % 10)
 nums = ['1', '2', '3']
 n_list = list(map(int, nums))  # [1, 2, 3]
 n_list = [int(item) for item in nums] # [1, 2, 3]
+
+# In Python 3 you can iterate over the lines of standard input, the loop will stop when EOF is reached:
+from sys import stdin
+
+for line in stdin:
+  print(line, end='')
 
 
 
