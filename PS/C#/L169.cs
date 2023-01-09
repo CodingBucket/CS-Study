2bfb72f74f837143009903745358a4eeab6ce2fd
Hasan Khan <hasan.khan.uap@gmail.com>
2023-01-10 03:37:05 +0600

    Create L169.cs

---
+++ PS/C#/L169.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        
+        // https://leetcode.com/problems/majority-element/description/
+
+        var num_len = nums.Count(); // 3
+
+        var max_el = num_len / 2; // 1
+
+        var dic = new Dictionary<int, int>();
+
+        for (var i = 0; i < num_len; i++)
+        {
+            if (dic.ContainsKey(nums[i]))
+                dic[nums[i]] = dic[nums[i]] + 1;
+            else
+                dic.Add(nums[i], 1);
+
+            if (dic[nums[i]] > max_el)
+                return nums[i];
+        }
+        return -1;
+    }
+}
