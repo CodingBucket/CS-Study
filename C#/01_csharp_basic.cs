d6ed076543b5e3d36b4ae76689b50a1032d2a466
Hasan Khan <hasan.khan.uap@gmail.com>
2023-01-10 13:07:53 +0600

    Create 01_csharp_basic.cs

---
+++ C#/01_csharp_basic.cs
@@ -0,0 +1,16 @@
+
+// Array
+
+var array = new int[] { 1, 2, 3};
+
+int[] myNum = {10, 20, 30, 40};
+
+
+// List
+
+var list = new List<int>{1, 2, 3};
+
+
+// Dictionary
+
+var dic = new Dictionary<int, int>();
