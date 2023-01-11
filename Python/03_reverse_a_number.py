28902b7934e61fc7366e87424424946341ee521a
Hasan Khan <hasan.khan.uap@gmail.com>
2023-01-12 00:45:38 +0600

    Update 03_reverse_a_number.py

--- Python/03_reverse_a_number.py
+++ Python/03_reverse_a_number.py
@@ -2,8 +2,8 @@ num = 1234
 reverse_num = 0
 
 while num != 0:
-    digit = num % 10
-    reverse_num = reverse_num * 10 + digit
+    reminder = num % 10
+    reverse_num = reverse_num * 10 + reminder
     num = num // 10
 
 print(reversed_num)
