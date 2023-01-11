d4db029eda3baeea7aab98bdb5c473bffe035bcf
Hasan Khan <hasan.khan.uap@gmail.com>
2023-01-12 02:14:58 +0600

    Create 04_palindrome_number.cs

---
+++ C#/04_palindrome_number.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPalindrome(int num) {
+        long rev_num = 0; // L9
+        var n = num;
+
+        if (n < 0)
+            return false;
+
+        while (num != 0)
+        {
+            var reminder = num % 10;
+            rev_num = rev_num * 10 + reminder;
+            num = num / 10;
+        }
+
+        return n == rev_num;
+    }
+}
