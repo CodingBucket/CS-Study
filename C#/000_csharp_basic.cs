d413e89d72148e3022bf5499f59bf85d3ef5c803
Hasan Khan <hasan.khan.uap@gmail.com>
2023-01-21 01:14:04 +0600

    Update 000_csharp_basic.cs

--- C#/000_csharp_basic.cs
+++ C#/000_csharp_basic.cs
@@ -3,13 +3,15 @@
 
 var array = new int[] { 1, 2, 3};
 
-Array.Sort(array);
+Array.Sort(str);     // Array sort
+Array.Clear(str);    // Clears the content of array
+Array.Reverse(str);  // Reverse the array
 
 var max = array.Max();
 
 var min = array.Min();
 
-int[] myNum = {10, 20, 30, 40};
+var binary = Convert.ToString(11, 2); // Int to binary
 
 
 // List
@@ -20,3 +22,8 @@ var list = new List<int>{1, 2, 3};
 // Dictionary
 
 var dic = new Dictionary<int, int>();
+
+
+var input = Console.ReadLine();
+
+Console.WriteLine("Radius: {0}, Area: {1}", 0, 1);
