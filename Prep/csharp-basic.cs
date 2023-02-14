c694a0da7de25549fd2d2a1ccbf9350006e8c790
Hasan Khan <hasan.khan.uap@gmail.com>
2023-02-15 00:56:03 +0600

    Update csharp-basic.cs

--- Prep/csharp-basic.cs
+++ Prep/csharp-basic.cs
@@ -1,1 +1,51 @@
+// Array
 
+var array = new int[] { 1, 2, 3};
+
+Array.Sort(str);     // Array sort
+Array.Clear(str);    // Clears the content of array
+Array.Reverse(str);  // Reverse the array
+
+var max = array.Max();
+
+var min = array.Min();
+
+var binary = Convert.ToString(11, 2); // Int to binary
+
+
+// List
+
+var list = new List<int>{1, 2, 3};
+
+
+// Dictionary
+
+var dic = new Dictionary<int, int>();
+
+// Set
+
+var hashSet = new HashSet<int>();
+hashSet.Contains();
+
+// Math
+
+var maxValue = Math.Max(firstVal, secondVal);
+
+Console.WriteLine(4 % 2); // 0
+Console.WriteLine(0 % 2); // 0
+Console.WriteLine(1 % 2); // 1
+
+Console.WriteLine(4 / 2); // 2
+Console.WriteLine(0 / 2); // 0
+Console.WriteLine(1 / 2); // 0.5 -> 0
+
+// Reverse String
+char[] stringArray = r.ToCharArray();
+Array.Reverse(stringArray);
+string reverseString = new string(stringArray);
+
+// Console
+
+var input = Console.ReadLine();
+
+Console.WriteLine("Radius: {0}, Area: {1}", 0, 1);
