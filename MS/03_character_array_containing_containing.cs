9ffd1b65d0ca685ba5a0e5a1eea3c4982119b529
Hasan Khan <hasan.khan.uap@gmail.com>
2023-03-20 03:50:13 +0600

    Update 03_character_array_containing_containing.cs

--- MS/03_character_array_containing_containing.cs
+++ MS/03_character_array_containing_containing.cs
@@ -1,23 +1,22 @@
-var array = new char[] {  };
-var start = 0;
-var len = 1;
+var a = new char[] {  };
+Solution(a, 0, 1);
 
-var arrayLen = array.Length;
-var result = new char[len];
+char[] Solution(char[] a, int start, int len)
+{
+    int total = len + start; int j = 0;
+    if (total > a.Length || start < 0 || len < -1) {
+        Console.WriteLine("Null"); return null;
+    }
+    var result = new char[len];
 
-if(start+len > arrayLen || start < 0 || len < 0)
-{
-    Console.WriteLine("null");
-}
-else
-{
-    var j = 0;
-    for (var i = start; i < start+len; i++)
-    {
-        result[j] = array[i];
+    for (int i=start; j<len; i++) {
+        result[j] = a[i];
         j++;
     }
 
-    foreach (var k in result)
-        Console.WriteLine(k);
+    foreach (char c in result) {
+        Console.WriteLine(c);
+    }
+
+    return result;
 }
